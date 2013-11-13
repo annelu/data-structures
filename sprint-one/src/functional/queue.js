bc7266c6dd19434296786c962937caf127314b1b
annelu <annelu@notalion.com>
2013-11-13 15:33:36 -0800

    Added push and size

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,21 @@ var makeQueue = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  var size = 0;
   // Implement the methods below
 
   instance.enqueue = function(value){
+    storage[size] = value;
+    size++;
   };
 
   instance.dequeue = function(){
+    return storage[first];
+    delete storage[first];
   };
 
   instance.size = function(){
+    return size;
   };
 
   return instance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,17 +3,20 @@ var makeStack = function(){
 
   // Use an object with numeric keys to store values
   var storage = {};
-  var size; // Hint: set an initial value here
+  var length = 0; // Hint: set an initial value here
 
   // Implement the methods below
 
   instance.push = function(value){
+    length++
+    storage[length] = value;
   };
 
   instance.pop = function(){
   };
 
   instance.size = function(){
+    return length;
   };
 
   return instance;

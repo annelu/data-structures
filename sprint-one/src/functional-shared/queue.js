d8794fa28b4b2e62e681f734cf2545e81f21f911
annelu <annelu@notalion.com>
2013-11-13 16:40:41 -0800

    Added size

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,30 @@
 var makeQueue = function(){
   // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+  instance.storage = {};
+  instance.qStart = 0;
+  instance.qEnd = 0;
+
+  instance.size = queueMethods.size;
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  /*enqueue: function(value){
+    storage[qEnd] = value;
+    qEnd++;
+  },
+
+  dequeue: function(){
+    if (qEnd - qStart) {
+      var firstIn = storage[qStart];
+      delete storage[qStart];
+      qStart++
+      return firstIn;
+    }
+  },*/
+
+  size: function(){
+    return this.qEnd - this.qStart;
+  }
+};

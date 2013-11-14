149a5f57c35ecad000483e9c49fd7ad9d377aa4a
annelu <annelu@notalion.com>
2013-11-13 16:29:54 -0800

    Added size and push

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,5 +1,30 @@
 var makeStack = function() {
   // Hey! Copy your code from src/functional/stack.js and paste it here
+  var instance = {};
+  instance.storage = {};
+  instance.length = 0; 
+
+  instance.size = stackMethods.size;
+  instance.push = stackMethods.push;
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value){
+    this.length++
+    this.storage[this.length] = value;
+  },
+
+  /*pop = function(){
+    if (length) {
+      var lastIn = storage[length];
+      delete storage[length];
+      length--
+      return lastIn;
+    }
+  }; */
+
+  size:  function(){
+    return this.length;
+  }
+};

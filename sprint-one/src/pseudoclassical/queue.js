b491516d8e90b6c0f57283f5dd9b2166d2929d00
annelu <annelu@notalion.com>
2013-11-13 20:00:59 -0800

    Now working with psuedoclassical requirements

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,24 @@
 var Queue = function() {
   // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.qStart = 0;
+  this.qEnd = 0;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.storage[this.qEnd] = value;
+  this.qEnd++;
+};
+
+Queue.prototype.dequeue = function(){
+  if (this.qEnd - this.qStart) {
+    var firstIn = this.storage[this.qStart];
+    delete this.storage[this.qStart];
+    this.qStart++
+    return firstIn;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.qEnd - this.qStart;
 };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,23 @@
 var Stack = function() {
   // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.length = 0;
+};
+
+Stack.prototype.push =  function(value){
+  this.length++;
+  this.storage[this.length] = value;
+};
+
+Stack.prototype.pop = function(){
+  if (this.length) {
+    var lastIn = this.storage[this.length];
+    delete this.storage[this.length];
+    this.length--;
+    return lastIn;
+  }
+};
+
+Stack.prototype.size = function(){
+    return this.length;
 };

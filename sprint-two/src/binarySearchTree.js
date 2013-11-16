27f792e47bb94cceceaa9fdff38890b9c09d0b6f
annelu <annelu@notalion.com>
2013-11-16 14:20:36 -0800

    binary search tree has methods contains, insert, and depthFirstLog

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,13 @@
 var makeBinarySearchTree = function(){
+  var instance = Object.create(binaryTreeMethods);
+
+  return instance;
 };
+
+var binaryTreeMethods = {};
+
+binaryTreeMethods.insert = function(){};
+
+binaryTreeMethods.contains = function(){};
+
+binaryTreeMethods.depthFirstLog = function(){};
